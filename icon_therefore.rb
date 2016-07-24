dbe7ff781d7f0d4a70ae210f0e7d19043a40a8d8
Garow oLFlex <galdstrings@gmail.com>
2016-07-24 20:12:04 +0900

    グラデーション、アイコンサイズ調整

--- icon_therefore.rb
+++ icon_therefore.rb
@@ -2,14 +2,14 @@ require 'cairo'
 
 WIDTH = 128
 HEIGHT = WIDTH
-ICON_SIZE = (WIDTH - 2.0) / 2.0
+ICON_SIZE = (WIDTH * 0.8) / 2.0
 CORE_SIZE = ICON_SIZE / 10.0
 
 PIECE_CNT = 3
 LINE_COLOR = "#00000099"
 PIECE_COLORS = [
-  ['#000066FF', '#FF0000FF'],
-  ['#333333FF', '#EEEEFFFF'],
+  ['#0033CCFF', '#FF0000FF'],
+  ['#000066FF', '#EEEEFFFF'],
   ['#EEEEFFFF', '#000066FF'],
 ]
 
@@ -45,6 +45,23 @@ PIECE_CNT.times do |cnt|
   end
 
   context.rotate(rot_to)
+end
+
+context.rotate(Math::PI / 2.0)
+
+LIGHTING_RAD = Math::PI * 5.0 / 4.0
+x, y = getpos.call(ICON_SIZE - 24.0, LIGHTING_RAD)
+s_grad = Cairo::RadialPattern.new(
+  0, 0, ICON_SIZE,
+  x, y, 0
+)
+s_grad.add_color_stop(0.0, '#00000099')
+s_grad.add_color_stop(0.3, '#3333FF33')
+s_grad.add_color_stop(0.5, '#3333FF00')
+
+context.fill do
+  context.circle(0, 0, ICON_SIZE)
+  context.set_source(s_grad)
 end
 
 surface.write_to_png('icon_therefore.png')
