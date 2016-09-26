a34eae91e4be442f2e22412cf01eb0bfce6c3286
Garow oLFlex <galdstrings@gmail.com>
2016-09-26 21:12:26 +0900

    各辺の色を虹色グラデーション + 内部動作調整

--- equilateral_polygon.rb
+++ equilateral_polygon.rb
@@ -4,12 +4,51 @@ IMAGE_SIZE = 1024
 RADIUS = (IMAGE_SIZE / 2.0) - 8.0
 
 BG_COLOR = '#FFFFFF'
-LINE_COLOR = '#333333'
+#LINE_COLOR = '#333333'
 LINE_WIDTH = 1.0
 
 if (corner_count = ARGV[0].to_i) < 3
   puts "三角形からしか使えません"
   exit 1
+end
+
+# 虹色ピッカー生成(引数: 目標カウント数)
+# 目標カウント数の周期で赤緑青への遷移を行う、色取得用のラムダ式を生成する
+def create_rainbow_picker(count)
+  ->(i) {
+    r = g = b = 0
+    phase_val = 6.0 * i / count
+    phase_val -= 6.0 if phase_val > 6.0
+    fraction = phase_val - phase_val.floor
+    case phase_val.to_i
+    when 0
+      # 赤MAX, 緑UP
+      r = 1.0
+      g = fraction
+    when 1
+      # 赤DOWN, 緑MAX
+      r = 1.0 - fraction
+      g = 1.0
+    when 2
+      # 緑MAX, 青UP
+      g = 1.0
+      b = fraction
+    when 3
+      # 緑DOWN, 青MAX
+      g = 1.0 - fraction
+      b = 1.0
+    when 4
+      # 青MAX, 赤UP
+      b = 1.0
+      r = fraction
+    when 5
+      # 青DOWN, 赤MAX
+      b = 1.0 - fraction
+      r = 1.0
+    end
+
+    [r, g, b]
+  }
 end
 
 # 初期化(背景色塗り + 原点を中央に移動 + 最初の頂点を上に)
@@ -23,17 +62,19 @@ context.rotate(-Math::PI / 2.0)
 # 描画準備
 per_rad = Math::PI * 2 / corner_count
 position = ->(r, i){ [r * Math.cos(i * per_rad), r * Math.sin(i * per_rad)] }
+picker = create_rainbow_picker(corner_count)
 
 # 描画
-context.stroke do
-  context.set_source_color(LINE_COLOR)
-  context.set_line_width(LINE_WIDTH)
+context.set_line_width(LINE_WIDTH)
 
-  corner_count.times do |c|
-    x, y = position[RADIUS, c]
-    context.line_to(x, y)
+corner_count.times do |c|
+  sx, sy = position[RADIUS, c]
+  dx, dy = position[RADIUS, c + 1]
+  context.stroke do
+    context.move_to(sx, sy)
+    context.line_to(dx, dy)
+    context.set_source_color(picker[c])
   end
-  context.close_path
 end
 
 # ファイルに出力
